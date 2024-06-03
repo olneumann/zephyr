06a6833fdb6f96837ae83a00815e089bcf352761
Oliver Sebastian Neumann <oliver.neumann@dlr.de>
2024-06-03 16:02:18 +0200

    Modified mlx90395.c and mlx90395.h files.

--- drivers/sensor/melexis/mlx90395/mlx90395.c
+++ drivers/sensor/melexis/mlx90395/mlx90395.c
@@ -20,59 +20,70 @@ static int mlx90395_sample_fetch(const struct device *dev, enum sensor_channel c
 {
 	const struct mlx90395_config *config = dev->config;
 	struct mlx90395_data *data = dev->data;
+	uint8_t cmd[7] = { 0 };
+	int ret = 0;
 
-	// uint8_t buffer[7] = {0};
+	cmd[0] = MLX90395_CMD_READ_MEASUREMENT | X_FLAG | Y_FLAG | Z_FLAG; // TODO: Individual channels
 
-	// k_sem_take(&data->sem, K_FOREVER);
+	k_sem_take(&data->sem, K_FOREVER);
 
-	// if (i2c_burst_read_dt(&config->i2c, mlx90395_XOUT_0, buffer, sizeof(buffer)) < 0) {
-	// 	LOG_ERR("Failed to read data sample");
-	// 	return -EIO;
-	// }
+	if (i2c_write_read_dt(&config->i2c, &cmd[0], 1, &cmd[0], 7) < 0) {
+		LOG_ERR("Failed to read data sample");
+		ret = -EIO;
+	}
 
-	// k_sem_give(&data->sem);
+	k_sem_give(&data->sem);
 
-	// data->magn_x = (uint32_t)(buffer[0] << 10 | buffer[1] << 2 | (buffer[6] & 0xC0) >> 6); // Turn the 18 bits into a unsigned 32-bit value
-	// data->magn_y = (uint32_t)(buffer[2] << 10 | buffer[3] << 2 | (buffer[6] & 0x30) >> 4); // Turn the 18 bits into a unsigned 32-bit value
-	// data->magn_z = (uint32_t)(buffer[4] << 10 | buffer[5] << 2 | (buffer[6] & 0x0C) >> 2); // Turn the 18 bits into a unsigned 32-bit value
+	data->magn_x = (uint16_t)(cmd[1] << 8 | cmd[2]);
+	data->magn_y = (uint16_t)(cmd[3] << 8 | cmd[4]);
+	data->magn_z = (uint16_t)(cmd[5] << 8 | cmd[6]);
+
+	return ret;
+}
+
+// Convert functions
+// XY
+static int mlx90395_convert_xy(struct sensor_value *val, uint32_t raw_val)
+{
+	int32_t value = raw_val / 140 * 1000;
+	val->val1 = value / 1000;
+	val->val2 = value % 1000;
+
+	return 0;
+}
+//Z
+static int mlx90395_convert_z(struct sensor_value *val, uint32_t raw_val)
+{
+	int32_t value = raw_val / 140 * 1000;
+	val->val1 = value / 1000;
+	val->val2 = value % 1000;
 
 	return 0;
 }
 
-static int mlx90395_convert(struct sensor_value *val, uint32_t raw_val)
-{
-	double value = sys_le16_to_cpu(raw_val);
-	value = (value - PARAM_NULLFIELD_18BIT) / PARAM_MAGN_LSB_GAUSS;
-	
-	return sensor_value_from_double(val, value);
-}
-
 static int mlx90395_channel_get(const struct device *dev, enum sensor_channel chan, struct sensor_value *val)
 {
-	int ret = 0;
 	struct mlx90395_data *data = dev->data;
+	int ret = 0;
 
 	k_sem_take(&data->sem, K_FOREVER);
 
-	// switch (chan) {
-	// 	case SENSOR_CHAN_MAGN_X:
-	// 		mlx90395_convert(val, data->magn_x);
-	// 		break;
-	// 	case SENSOR_CHAN_MAGN_Y:
-	// 		mlx90395_convert(val, data->magn_y);
-	// 		break;
-	// 	case SENSOR_CHAN_MAGN_Z:
-	// 		mlx90395_convert(val, data->magn_z);
-	// 		break;
-	// 	case SENSOR_CHAN_MAGN_XYZ:
-	// 		mlx90395_convert(&val[0], data->magn_x);
-	// 		mlx90395_convert(&val[1], data->magn_y);
-	// 		mlx90395_convert(&val[2], data->magn_z);
-	// 		break;
-	// 	default:
-	// 		LOG_ERR("Unsupported channel");
-	// 		ret = -ENOTSUP;	
-	// }
+	switch (chan) {
+		case SENSOR_CHAN_MAGN_X:
+			break;
+		case SENSOR_CHAN_MAGN_Y:
+			break;
+		case SENSOR_CHAN_MAGN_Z:
+			break;
+		case SENSOR_CHAN_MAGN_XYZ:
+			mlx90395_convert_xy(&val[0], data->magn_x);
+			mlx90395_convert_xy(&val[1], data->magn_y);
+			mlx90395_convert_z(&val[2], data->magn_z);
+			break;
+		default:
+			LOG_ERR("Unsupported channel");
+			ret = -ENOTSUP;	
+	}
 
 	k_sem_give(&data->sem);
 
@@ -84,15 +95,63 @@ static const struct sensor_driver_api mlx90395_driver_api = {
 	.channel_get = mlx90395_channel_get,
 };
 
-// static int mlx90395_set_internal_control_0(const struct device *dev, bool auto_sr_en, bool drdy_interrupt)
-// {
-// 	const struct mlx90395_config *config = dev->config;
-// 	uint8_t reg = 0;
+static int mlx90395_exit(const struct device *dev)
+{
+	const struct mlx90395_config *config = dev->config;
+	uint8_t cmd = MLX90395_CMD_EXIT;
 
-// 	reg |= (auto_sr_en << BIT_AUTO_SR_EN) | (drdy_interrupt << BIT_DRDY_INTERRUPT);
+	return i2c_write_read_dt(&config->i2c, &cmd, 1, &cmd, 1);
+}
 
-// 	return i2c_reg_write_byte_dt(&config->i2c, mlx90395_CONTROL_0, reg);
-// }
+static int mlx90395_reset(const struct device *dev)
+{
+	const struct mlx90395_config *config = dev->config;
+	uint8_t cmd = MLX90395_CMD_RESET;
+
+	return i2c_write_read_dt(&config->i2c, &cmd, 1, &cmd, 1);
+}
+
+static int mlx90395_write_register(const struct device *dev, uint8_t reg, uint16_t data)
+{
+	const struct mlx90395_config *config = dev->config;
+	uint8_t cmd[3] = { 0 };
+
+	cmd[0] = (reg & 0x7f) << 1;
+	cmd[1] = (data & 0xff00) >> 8;
+	cmd[2] = 	data & 0x00ff;
+
+	return i2c_write_read_dt(&config->i2c, cmd, 3, cmd, 1);
+}
+
+static int mlx90395_start_burst(const struct device *dev, uint8_t zyxt_flags)
+{
+	const struct mlx90395_config *config = dev->config;
+	uint8_t cmd = MLX90395_CMD_START_BURST | zyxt_flags;
+
+	return i2c_write_read_dt(&config->i2c, &cmd, 1, &cmd, 1);
+}
+
+static int mlx90395_set_mode(const struct device *dev, uint8_t zyxt_flags, enum mlx90395_mode mode, uint8_t bdr)
+{
+	switch (mode) {
+		case MLX90395_MODE_IDLE:
+			mlx90395_exit(dev);
+			break;
+		case MLX90395_MODE_BURST:
+			mlx90395_write_register(dev, BURST_SEL_REG, bdr);
+			mlx90395_start_burst(dev, zyxt_flags);
+			break;
+		case MLX90395_MODE_SINGLE_MEASUREMENT:
+			// TODO: Implement
+			break;
+		case MLX90395_MODE_WAKE_ON_CHANGE:
+			// TODO: Implement
+			break;
+		default:
+			LOG_ERR("Invalid mode");
+			return -EINVAL;
+	}
+}
 
 static int mlx90395_init(const struct device *dev)
 {
@@ -104,11 +163,32 @@ static int mlx90395_init(const struct device *dev)
 		return -ENODEV;
 	}
 
-	// /* Set internal control 0 */
-	// if (mlx90395_set_internal_control_0(dev, true, false) < 0) {
-	// 	LOG_ERR("Failed to set internal control 0");
-	// 	return -EIO;
-	// }
+	/* Exit the sensor */
+	if (mlx90395_exit(dev) < 0) {
+		LOG_ERR("Failed to exit sensor");
+		return -EIO;
+	}
+
+	k_msleep(50);
+
+	/* Reset the sensor */
+	if (mlx90395_reset(dev) < 0) {
+		LOG_ERR("Failed to reset sensor");
+		return -EIO;
+	}
+
+	k_msleep(50);
+
+	/* Set mode */
+	uint8_t zyxt_flags_tmp = X_FLAG | Y_FLAG | Z_FLAG;
+	uint8_t bdr_tmp = 1;
+
+	if (mlx90395_set_mode(dev, zyxt_flags_tmp, MLX90395_MODE_BURST, bdr_tmp) < 0) {
+		LOG_ERR("Failed to set mode");
+		return -EIO;
+	}
+
+	k_msleep(20);
 
 	/* Initialize semaphore */
 	k_sem_init(&data->sem, 1, 1);
--- drivers/sensor/melexis/mlx90395/mlx90395.h
+++ drivers/sensor/melexis/mlx90395/mlx90395.h
@@ -9,6 +9,37 @@
 
 #include <zephyr/drivers/i2c.h>
 
+enum { STATUS_OK = 0, STATUS_ERROR = 0xff } return_status_t;
+enum { Z_FLAG = 0x8, Y_FLAG = 0x4, X_FLAG = 0x2, T_FLAG = 0x1 } axis_flag_t;
+enum { I2C_BASE_ADDR = 0x0c };
+enum { GAIN_SEL_REG = 0x0, GAIN_SEL_MASK = 0x0070, GAIN_SEL_SHIFT = 4 };
+enum { HALLCONF_REG = 0x0, HALLCONF_MASK = 0x000f, HALLCONF_SHIFT = 0 };
+enum { BURST_SEL_REG = 0x1, BURST_SEL_MASK = 0x03c0, BURST_SEL_SHIFT = 6};
+enum { TRIG_INT_SEL_REG = 0x1, TRIG_INT_SEL_MASK = 0x8000, TRIG_INT_SEL_SHIFT = 15 };
+enum { EXT_TRIG_REG = 0x1, EXT_TRIG_MASK = 0x0800, EXT_TRIG_SHIFT = 11 };
+enum { OSR_REG = 0x2, OSR_MASK = 0x0003, OSR_SHIFT = 0 };
+enum { OSR2_REG = 0x2, OSR2_MASK = 0x1800, OSR2_SHIFT = 11 };
+enum { DIG_FLT_REG = 0x2, DIG_FLT_MASK = 0x001c, DIG_FLT_SHIFT = 2 };
+enum { RES_XYZ_REG = 0x2, RES_XYZ_MASK = 0x07e0, RES_XYZ_SHIFT = 5 };
+enum { TCMP_EN_REG = 0x1, TCMP_EN_MASK = 0x0400, TCMP_EN_SHIFT = 10 };
+enum { X_OFFSET_REG = 6, Y_OFFSET_REG = 7, Z_OFFSET_REG = 8 };
+enum { WOXY_THRESHOLD_REG = 0x0E, WOZ_THRESHOLD_REG = 0x0F, WOTT_THRESHOLD_REG = 0x10, WOVT_THRESHOLD_REG = 0x11 };
+enum { BURST_MODE_BIT = 0x80, WAKE_ON_CHANGE_BIT = 0x40,
+		POLLING_MODE_BIT = 0x20, ERROR_BIT = 0x10, EEC_BIT = 0x08,
+		RESET_BIT = 0x04, D1_BIT = 0x02, D0_BIT = 0x01 };
+
+enum {
+	MLX90395_CMD_NOP 				= 0x00, /* No operation */
+	MLX90395_CMD_START_BURST		= 0x10, /* Start burst mode */
+	MLX90395_CMD_WAKE_ON_CHANGE		= 0x20, /* Start wake-up on change mode */
+	MLX90395_CMD_START_MEASUREMENT	= 0x30, /* Start single measurement (polling mode) */
+	MLX90395_CMD_READ_MEASUREMENT	= 0x80, /* Read measurement, 0x40 for MLX90393 */
+	MLX90395_CMD_EXIT				= 0x80, /* Exit */
+	MLX90395_CMD_MEMORY_RECALL		= 0xD0, /* Memory recall */
+	MLX90395_CMD_MEMORY_STORE		= 0xE0, /* Memory store */
+	MLX90395_CMD_RESET				= 0xF0, /* Reset */	
+};
+
 #define MLX90395_I2C_ADDR      0x30
 
 #define MLX90395_XOUT_0        0x00
@@ -80,11 +111,21 @@ enum mlx90395_prd_set {
 	MLX90395_DT_PRD_SET_2000ms,
 };
 
+enum mlx90395_mode {
+	MLX90395_MODE_IDLE,
+	MLX90395_MODE_BURST,
+	MLX90395_MODE_SINGLE_MEASUREMENT,
+	MLX90395_MODE_WAKE_ON_CHANGE,
+};
+
 struct mlx90395_config {
 	struct i2c_dt_spec i2c;
 	uint16_t bandwidth;
 	uint16_t frequency;
 	uint16_t prd_set;
+	uint8_t zyxt;
+	uint8_t bdr;
+	enum mlx90395_mode mode;
 };
 
 struct mlx90395_data {
